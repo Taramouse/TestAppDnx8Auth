10aa773e6e24922883c2f585d1aee5e558b5e3af
Taramouse <taramouse@live.co.uk>
2015-11-11 15:32:28 +0000

    Initial Project Created. Not yet built but dependancies added ok.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TestAppDnx8Auth
 
-ASP.NET DNX v1.0.0-beta8 project with authentication, created in Visual Studio 2015.
+ASP.NET MVC 6 EF7 DNX v1.0.0-beta8 project with Individual User Accounts authentication, created in Visual Studio 2015.
---
+++ TestAppDnx8Auth/src/TestAppDnx8Auth/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestAppDnx8Auth")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAppDnx8Auth")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("89bc9dbc-b0b9-4247-9e07-38a5e267d052")]
